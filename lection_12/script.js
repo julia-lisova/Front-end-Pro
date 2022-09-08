fab4766d808dd63b3a08160a561a58ff9615ffbc
julia-lisova <khvesik@gmail.com>
2022-09-08 15:01:37 +0300

    HW: "Cards" with array

--- lection_12/script.js
+++ lection_12/script.js
@@ -4,43 +4,45 @@ const PEPSON = {
     'queen': 'Q',
     'king': 'K',
 }
-let result_2to10 = [];
-let resultPerson = [];
+const result_2to10 = [];
+const resultPerson = [];
 
 function getCard(cardsSuit, whichCard) {
-    let a = `<div class="card__info">${whichCard}<img src="images/${cardsSuit}.svg" alt="${cardsSuit}"></div>`;
+    const a = `<div class="card__info">${whichCard}<img src="images/${cardsSuit}.svg" alt="${cardsSuit}"></div>`;
     return a + a;
 }
 
-function getCard_person(cardsSuit, whichCard) {
-    let a = `<div class="card__info">${PEPSON[whichCard] || 'T'}<img src="images/${cardsSuit}.svg" alt="${cardsSuit}"></div>`;
-    return `${a}${`<img class="person" src="images/${whichCard || cardsSuit}.svg" alt="${cardsSuit}">`}${a}`;
+function getCardPerson(cardsSuit, whichCard) {
+    const PERSON_Entries = Object.entries(PEPSON).find(el => el[0] === whichCard);
+    const a = `<div class="card__info">${PERSON_Entries && PERSON_Entries[1] || 'T'}<img src="images/${cardsSuit}.svg" alt="${cardsSuit}"></div>`;
+    // const a = `<div class="card__info">${PEPSON[whichCard] || 'T'}<img src="images/${cardsSuit}.svg" alt="${cardsSuit}"></div>`;
+
+    return a + `<img class="person" src="images/${whichCard || cardsSuit}.svg" alt="${cardsSuit}">` + a;
 }
 
 for (let i = 2; i <= 14; i++) {
     for (let j of CARD__SUIT) {
 
         switch (true) {
-
         case 2 <= i && i <= 10:
             result_2to10.push(`<div class="card">${getCard(j, i)}</div>`);
             break;
         case i === 11:
-            resultPerson.push(`<div class="card card--person">${getCard_person(j, 'jack')}</div>`);
+            resultPerson.push(`<div class="card card--person">${getCardPerson(j, 'jack')}</div>`);
             break;
         case i === 12:
-            resultPerson.push(`<div class="card card--person">${getCard_person(j, 'queen')}</div>`);
+            resultPerson.push(`<div class="card card--person">${getCardPerson(j, 'queen')}</div>`);
             break;
         case i === 13:
-            resultPerson.push(`<div class="card card--person">${getCard_person(j, 'king')}</div>`);
+            resultPerson.push(`<div class="card card--person">${getCardPerson(j, 'king')}</div>`);
             break;
         case i === 14:
-            resultPerson.push(`<div class="card card--person">${getCard_person(j)}</div>`);
+            resultPerson.push(`<div class="card card--person">${getCardPerson(j)}</div>`);
             break;
         }
     }
 }
 
- let randerCard = (arr1, arr2) =>`<div class="wrapper">${arr1.join('') + arr2.join('')}</div>`;
-document.write(randerCard(result_2to10, resultPerson));
+let randerCard = (arr1, arr2) => `<div class="wrapper">${arr1.join('') + arr2.join('')}</div>`;
 
+document.write(randerCard(result_2to10, resultPerson));
